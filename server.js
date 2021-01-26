3ebd17522845e050d400cb46017e379332f74b4c
ocktopusPrime <jared.long.d@gmail.com>
2021-01-25 20:49:21 -0700

    playing with package.json

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, 'client/build');
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port}.`));
 
 app.use(express.static(publicPath));
 
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 
-app.listen(port, () => {
-	console.log('Server is up!');
-});
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
