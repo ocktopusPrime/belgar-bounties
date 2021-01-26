6b5f9513fca883516702a3e218e65c07b9ebe31c
ocktopusPrime <jared.long.d@gmail.com>
2021-01-25 22:28:32 -0700

    removed static

--- server.js
+++ server.js
@@ -5,6 +5,4 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server up and running on port ${port}.`));
 
-app.use('/static', express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '/client/build/index/html')));
+app.use(express.static(path.join(__dirname, 'client/build')));
