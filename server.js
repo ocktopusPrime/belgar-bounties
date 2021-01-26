5dfc5a4676ae803129ad2d0be2e8c4af6719081e
ocktopusPrime <jared.long.d@gmail.com>
2021-01-26 10:38:36 -0700

    added listener

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.listen(port, () => console.log(`Server up and running on port ${port}.`));
