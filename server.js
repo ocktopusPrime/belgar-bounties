9806887d5235cc9265b61770190e72229d96bb73
ocktopusPrime <jared.long.d@gmail.com>
2021-01-25 22:29:10 -0700

    moved listen

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server up and running on port ${port}.`));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.listen(port, () => console.log(`Server up and running on port ${port}.`));
