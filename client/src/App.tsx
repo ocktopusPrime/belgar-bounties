c081b7cffad8e304a1f67d65320a537b4e00ac10
ocktopusPrime <jared.long.d@gmail.com>
2021-01-25 19:55:16 -0700

    restructured!

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p style={{ fontFamily: 'cursive' }}>Welcome to Belgar Bounties!</p>
+			</header>
+		</div>
+	);
+}
