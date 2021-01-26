580b0903970e275936ad6a57763f435bbc91d855
ocktopusPrime <jared.long.d@gmail.com>
2021-01-25 21:19:42 -0700

    maybe? probs not

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server up and running on port ${port}.`));
 
 app.use(express.static(publicPath));
